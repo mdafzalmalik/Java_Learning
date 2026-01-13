d284103ec1ba8e453ee7284e63f400179fd12225
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2026-01-13 21:44:22 +0530

    2D Array Basic

---
+++ two_d_array/TwoDArray.java
@@ -0,0 +1,18 @@
+package two_d_array;
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = new int[3][4];
+		System.out.println(arr);
+		
+		System.out.println(arr.length); // row length
+		System.out.println(arr[0].length); // column length
+		
+		System.out.println(arr[1][2]);
+		
+
+	}
+
+}
---
+++ two_d_array/UserInput2D.java
@@ -0,0 +1,34 @@
+package two_d_array;
+
+import java.util.Scanner;
+
+public class UserInput2D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[][] arr = new int[n][m];
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		Display(arr);
+
+	}
+
+	public static void Display(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
