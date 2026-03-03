1370f2edd9346671be863db6537655e0d8a95a28
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2026-03-04 00:22:49 +0530

    update in sortings

--- sorting_algorithms/BubbleSort.java
+++ sorting_algorithms/BubbleSort.java
@@ -7,7 +7,7 @@ public class BubbleSort {
 		int[] arr = {2,5,9,1,3,0};
 		sort(arr);
 		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
+			System.out.print(arr[i] + " ");
 		}
 
 	}
